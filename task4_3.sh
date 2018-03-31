365c508c7e231f8358f6e27b05383efff915cd3b
Illia Manshyn <garterfild14@gmail.com>
2018-03-31 18:47:24 +0300

    Script for creating backups

---
+++ task4_3.sh
@@ -0,0 +1,39 @@
+#!bin/bash
+
+waytosave=/tmp/backups
+
+if ! [ -d $waytosave ]
+then
+mkdir $waytosave
+fi
+
+OLDESTBAK=$(ls -t "$waytosave" | grep "$NAMEOFBAKARCH**" | tail -n 1)
+SOURCE="$1"
+NUMBAK=$2
+NAMEOFBAKARCH=$(echo "$SOURCE" | sed 's/\///1' | sed 's/\//-/g' | sed 's/^-//' )
+NUMOFBAKEXISTS=$(ls -ltu $waytosave | grep "$NAMEOFBAKARCH**" | wc -l)
+
+if [[ $# -ne 2 ]]
+then
+echo "ERROR: Incorrect number of args" >&2
+exit 1
+fi
+
+if ! [ -d "$1" ]
+then
+echo "ERROR: '$1' It's not a directory" >&2
+exit 2
+fi
+
+if ! [[ $2 =~ "^[0-9]+$" ]] 
+then
+echo "error: $2 Not a number" >&2 
+exit 3
+fi
+
+cd $waytosave
+tar  -zcpf "$NAMEOFBAKARCH""$(date +%Y-%m-%d-%H%M%S)".tar.gz $SOURCE 2>/dev/null
+
+ls  $waytosave | grep "$NAMEOFBAKARCH**" | sort -n | head -n -$NUMBAK | sed "s/.*/\"&\"/" | xargs rm -f
+
+exit 0
